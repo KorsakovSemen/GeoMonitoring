8eae042051425b1f0bf1665d5fdbe3f05a957cf8
pako2 <deacle2009@mail.ru>
2020-04-20 16:19:26 +0700

    фикс вывода матрицы

--- SystAnalys_lr1/Forms/Matrix.cs
+++ SystAnalys_lr1/Forms/Matrix.cs
@@ -31,100 +31,106 @@ namespace SystAnalys_lr1
         }
 
         SortedDictionary<string, List<Vertex>> routesSorted;
-
+        int parkSize;
         private void Matrix()
         {
-
-            matrixGrid.Rows.Clear();
-            matrixGrid.Refresh();
-
-            buses.Sort((a1, b1) => a1.route.CompareTo(b1.route));
-
-            SplitBuses();
-
-            routesSorted = new SortedDictionary<string, List<Vertex>>(routes);
-
-            foreach (var x in routes)
+            if (parkSize + 1 < 655)
             {
-                if (x.Value.Count == 0)
+                matrixGrid.Rows.Clear();
+                matrixGrid.Refresh();
+
+                buses.Sort((a1, b1) => a1.route.CompareTo(b1.route));
+
+                SplitBuses();
+
+                routesSorted = new SortedDictionary<string, List<Vertex>>(routes);
+
+                foreach (var x in routes)
                 {
-                    routesSorted.Remove(x.Key);
-                }
-            }
-
-            int parkSize = 0;
-
-            foreach (var x in busesPark)
-            {
-                parkSize = Math.Max(parkSize, x.Count);
-            }
-
-            int[,] myArr = new int[routesSorted.Count, parkSize];
-
-            if (routesSorted.Count == 0)
-                matrixGrid.RowCount = 1;
-            else
-                matrixGrid.RowCount = routesSorted.Count;
-            matrixGrid.ColumnCount = parkSize + 1;
-
-            for (int i = 1; i < parkSize; i++)
-            {
-                matrixGrid.Columns[i - 1].HeaderText = i.ToString();
-                if (i + 1 == parkSize)
-                {
-                    matrixGrid.Columns[i].HeaderText = parkSize.ToString();
-                }
-            }
-
-            matrixGrid.Columns[parkSize].HeaderText = "Total";
-            try
-            {
-                for (int i = 0; i < busesPark.Count; ++i)
-                {
-                    if (busesPark[i].Count != 0)
+                    if (x.Value.Count == 0)
                     {
-                        matrixGrid.Rows[i].HeaderCell.Value = busesPark[i].Last().route.ToString();
+                        routesSorted.Remove(x.Key);
                     }
                 }
 
-                int total, res;
-                res = 0;
-                for (int i = 0; i < busesPark.Count; i++)
+                parkSize = 0;
+
+                foreach (var x in busesPark)
                 {
-                    total = 0;
-                    for (int j = 0; j < parkSize + 1; j++)
+                    parkSize = Math.Max(parkSize, x.Count);
+                }
+
+                int[,] myArr = new int[routesSorted.Count, parkSize];
+                if (routesSorted.Count == 0)
+                    matrixGrid.RowCount = 1;
+                else
+                    matrixGrid.RowCount = routesSorted.Count;
+
+                matrixGrid.ColumnCount = parkSize + 1;
+
+                for (int i = 1; i < parkSize; i++)
+                {
+                    matrixGrid.Columns[i - 1].HeaderText = i.ToString();
+                    if (i + 1 == parkSize)
                     {
-                        if (j < busesPark[i].Count)
+                        matrixGrid.Columns[i].HeaderText = parkSize.ToString();
+                        matrixGrid.Columns[i].FillWeight = 1;
+                    }
+                }
+
+                matrixGrid.Columns[parkSize].HeaderText = "Total";
+                try
+                {
+                    for (int i = 0; i < busesPark.Count; ++i)
+                    {
+                        if (busesPark[i].Count != 0)
                         {
-                            if (busesPark[i][j].tracker == true)
+                            matrixGrid.Rows[i].HeaderCell.Value = busesPark[i].Last().route.ToString();
+                        }
+                    }
+
+                    int total, res;
+                    res = 0;
+                    for (int i = 0; i < busesPark.Count; i++)
+                    {
+                        total = 0;
+                        for (int j = 0; j < parkSize + 1; j++)
+                        {
+                            if (j < busesPark[i].Count)
                             {
-                                myArr[i, j] = 1;
-                                total++;
+                                if (busesPark[i][j].tracker == true)
+                                {
+                                    myArr[i, j] = 1;
+                                    total++;
+                                }
+                                else
+                                {
+                                    myArr[i, j] = 0;
+                                }
+                                matrixGrid.Rows[i].Cells[j].Value = myArr[i, j];
                             }
                             else
                             {
-                                myArr[i, j] = 0;
+                                matrixGrid.Rows[i].Cells[j].Value = 0;
                             }
-                            matrixGrid.Rows[i].Cells[j].Value = myArr[i, j];
-                        }
-                        else
-                        {
-                            matrixGrid.Rows[i].Cells[j].Value = 0;
-                        }
-                        matrixGrid.Rows[i].Cells[parkSize].Value = total;
+                            matrixGrid.Rows[i].Cells[parkSize].Value = total;
 
+                        }
+                        res += total;
                     }
-                    res += total;
-                }
 
-                label8.Text = MainStrings.matrixFirst + res.ToString() + " " + MainStrings.matrixSecond + (buses.Count - res).ToString();
-                metroLabel7.Text = MainStrings.matrixThird + buses.Count.ToString();
+                    label8.Text = MainStrings.matrixFirst + res.ToString() + " " + MainStrings.matrixSecond + (buses.Count - res).ToString();
+                    metroLabel7.Text = MainStrings.matrixThird + buses.Count.ToString();
+                }
+                catch
+                {
+                    MetroMessageBox.Show(this, MainStrings.Matrix, MainStrings.error, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
             }
-            catch
+            else
             {
                 MetroMessageBox.Show(this, MainStrings.Matrix, MainStrings.error, MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
-
         }
     }
 }
